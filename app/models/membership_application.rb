f2c6d4dcd940a3f899defbf528accae98a7ffe36
Ethan Vizitei <ethan.vizitei@gmail.com>
2011-01-29 10:27:50 -0600

    handle SSN validation at the model level
    Signed-off-by: Ethan Vizitei <ethan.vizitei@gmail.com>

--- app/models/membership_application.rb
+++ app/models/membership_application.rb
@@ -1,2 +1,10 @@
 class MembershipApplication < ActiveRecord::Base
+  validates_presence_of :social_security_number
+  validates_numericality_of :social_security_number
+  validates_length_of :social_security_number,:is=>9
+  
+  def social_security_number=(value)
+    value = value.gsub(/[\s|-]+/,"") if value
+    write_attribute(:social_security_number,value)
+  end
 end
---
+++ spec/models/membership_application_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe MembershipApplication do
+  it {should validate_presence_of :social_security_number}
+
+  describe "ssn validation" do
+     it {should allow_value("123456789").for(:social_security_number)}
+     it {should_not allow_value("Z2345678A").for(:social_security_number)}
+     it {should_not allow_value("12345678912").for(:social_security_number)}
+     it {should_not allow_value("1234567").for(:social_security_number)}
+   end
+   
+   describe "ssn manipulation" do
+     describe "removing whitespace" do
+       it "strips from ends" do
+         MembershipApplication.new(:social_security_number=>"  12345  ").social_security_number.should == "12345"
+       end
+       
+       it "removes from center" do
+         MembershipApplication.new(:social_security_number=>"1 2 3 4 5").social_security_number.should == "12345"
+       end
+     end
+     
+     it "removes dashes" do
+       MembershipApplication.new(:social_security_number=>"1-2-34-5").social_security_number.should == "12345"
+     end
+   end
+end
