09acb819ab995f45ff168c34123601dcd9298f34
Ethan Vizitei <ethan.vizitei@gmail.com>
2011-01-30 18:04:52 -0600

    implement the "open" position finder
    Signed-off-by: Ethan Vizitei <ethan.vizitei@gmail.com>

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,3 +1,9 @@
 class Position < ActiveRecord::Base
   validates_presence_of :name
+  
+  class << self
+    def open
+      where(:active=>true)
+    end
+  end
 end
--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -2,4 +2,15 @@ require 'spec_helper'
 
 describe Position do
   it{ should validate_presence_of :name}
+  
+  describe "open finder" do
+    before(:each) do
+      @open_position = Factory(:open_position)
+      @closed_position = Factory(:closed_position)
+    end
+    
+    subject{ Position.open.all }
+    it { should include(@open_position) }
+    it { should_not include(@closed_position) }
+  end
 end
