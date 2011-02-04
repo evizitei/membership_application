e463b350abe3cabcad5f3d92fc1fc520b177ab6d
Ethan Vizitei <ethan.vizitei@gmail.com>
2011-02-04 11:48:00 -0600

    implement the position selection prior to applying
    Signed-off-by: Ethan Vizitei <ethan.vizitei@gmail.com>

--- app/controllers/membership_applications_controller.rb
+++ app/controllers/membership_applications_controller.rb
@@ -1,10 +1,12 @@
 class MembershipApplicationsController < ApplicationController
   def new
-    @membership_application = MembershipApplication.new
+    @position = Position.find(params[:position_id])
+    @membership_application = MembershipApplication.new(:position=>@position)
   end
   
   def create
-    @membership_application = MembershipApplication.new(params[:membership_application])
+    @position = Position.find(params[:position_id])
+    @membership_application = MembershipApplication.new(params[:membership_application].merge({:position_id=>@position.id}))
     render :action=>:new unless @membership_application.save
   end
 end
