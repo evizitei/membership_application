37b6d085076f50c3182351695dd333e36b227417
Ethan Vizitei <ethan.vizitei@gmail.com>
2011-02-19 19:42:29 -0600

    add a state machine
    Signed-off-by: Ethan Vizitei <ethan.vizitei@gmail.com>

--- app/models/membership_application.rb
+++ app/models/membership_application.rb
@@ -1,4 +1,8 @@
+require 'transitions'
+
 class MembershipApplication < ActiveRecord::Base
+  include ActiveRecord::Transitions
+  
   belongs_to :position
   
   attr_encrypted :social_security_number, :key => 's8L3P4t569An83FM5o2569vfAxeY5gV6'
@@ -9,7 +13,27 @@ class MembershipApplication < ActiveRecord::Base
   validates_inclusion_of :has_crime_convictions,:in=>[true,false]
   
   before_validation :format_ssn
+  
+  state_machine do
+    state :filling_out 
+    state :submitted
+    state :reviewed
 
+    event :submit do
+      transitions :to => :submitted, :from => :filling_out
+    end
+     
+  end
+  
+  class << self
+    def pending_review
+      where(:state=>"submitted")
+    end
+  end
+
+  def applicant_name
+    "#{first_name} #{last_name}"
+  end
 protected 
   def format_ssn
     self.social_security_number= social_security_number.gsub(/[\s|-]+/,"") if social_security_number
