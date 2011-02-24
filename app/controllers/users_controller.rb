e5a677e53a05aff5d0bcc4261afbe4a59c1ac306
Ethan Vizitei <ethan.vizitei@gmail.com>
2011-02-23 20:41:44 -0600

    users controller boilerplate
    Signed-off-by: Ethan Vizitei <ethan.vizitei@gmail.com>

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def edit
+    @user = current_user
+  end
+  
+  def update
+    @user = current_user
+    if @user.update_attributes(params[:user])
+      redirect_to root_path
+    else
+      render :action=>:edit
+    end
+  end
+end
