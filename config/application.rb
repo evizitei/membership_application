4845d5e98a011fcbceffb609591c7e5d567d9bf5
Ethan Vizitei <ethan.vizitei@gmail.com>
2011-02-26 17:33:10 -0600

    no more middleware
    Signed-off-by: Ethan Vizitei <ethan.vizitei@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,5 @@ module Membership
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-    config.middleware.use PDFKit::Middleware
   end
 end
