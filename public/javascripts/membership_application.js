46b55ee50f9b4cd2e4c647f3606bc7fa920f17ac
Ethan Vizitei <ethan.vizitei@gmail.com>
2011-07-11 14:13:57 -0500

    format date fields for skills and certs
    Signed-off-by: Ethan Vizitei <ethan.vizitei@gmail.com>

--- public/javascripts/membership_application.js
+++ public/javascripts/membership_application.js
@@ -1,4 +1,9 @@
 $(function(){
+  dialog_init();
+  date_selector_init();
+});
+
+function dialog_init(){
   if($("#errorExplanation").length == 0){
     if($(".modal").length > 0){
       $(".modal").dialog({
@@ -7,4 +12,8 @@ $(function(){
       });
     }
   }
-});
+}
+
+function date_selector_init(){
+  $(".datepicker").datepicker();
+}
